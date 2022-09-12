b33f1b7c8c3d889e758c33379cd6fb7bc63994c2
biratdatta <birat.datta26@gmail.com>
2022-09-12 21:07:42 +0530

    changes

--- main.kt
+++ main.kt
@@ -12,3 +12,35 @@ Hello, Android!
 The above code is a simple Kotlin program that prints Hello, Android! on the console.
 
 Kotlin is a statically typed programming language. It means that the compiler checks the type of the variable at compile time. The compiler throws an error if the variable is assigned a value of a different type.
+
+
+// Kotlin program to illustrate the use of var and val
+
+fun main() {
+    var name = "Android"
+    name = "Kotlin"
+    println(name)
+}
+
+// Output
+
+Kotlin
+
+The above code is a simple Kotlin program that illustrates the use of var and val. The var keyword is used to declare a variable. The val keyword is used to declare a constant. The value of a constant cannot be changed once it is assigned. The value of a variable can be changed.
+
+
+// Kotlin program to illustrate the use of var and val
+
+fun main() {
+    var name = "Android"
+    name = "Kotlin"
+    println(name)
+}
+
+// Output
+
+Kotlin
+
+
+// Kotlin program to illustrate the use of var and val
+
