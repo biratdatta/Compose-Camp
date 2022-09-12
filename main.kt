0e613d4a7d0f1f1adebafdbaf1edb655c2cd05b1
biratdatta <birat.datta26@gmail.com>
2022-09-12 21:05:47 +0530

    changes

---
+++ main.kt
@@ -0,0 +1,14 @@
+
+// Hello World Program
+
+fun main() {
+    println("Hello, Android!")
+}
+
+// Output
+
+Hello, Android!
+
+The above code is a simple Kotlin program that prints Hello, Android! on the console.
+
+Kotlin is a statically typed programming language. It means that the compiler checks the type of the variable at compile time. The compiler throws an error if the variable is assigned a value of a different type.
